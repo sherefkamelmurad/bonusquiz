3eaab6b470430983563d8a08bbe2b112bcbbb433
sherefkamelmurad <74113081+sherefkamelmurad@users.noreply.github.com>
2022-08-17 22:58:23 +0200

    Add files via upload

---
+++ jsbonusquiz/q2,1.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse(); 
+    var joinArray = reverseArray.join(""); 
+    return joinArray;
+} 
+reverseString("hello");
